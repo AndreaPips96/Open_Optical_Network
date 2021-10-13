cf5814ea2d1949fcec51b7043f730633dbac0ff2
AndreaPips96 <a.pipino@studenti.polito.it>
2021-10-13 11:26:07 +0200

    gitignore & Lab01 - first two ex

---
+++ Lab01/Ex01.py
@@ -0,0 +1,26 @@
+# Excercise Set 1 -> Python basics #
+
+# 1
+print("Insert two integer numbers")
+var1 = int(input("Insert first number: "))
+var2 = int(input("Insert second number: "))
+print("You inserted", var1, " and ", var2)
+
+if (var1 * var2) > 1000:
+    sum = var1 + var2
+    print(sum)
+else:
+    print("Their product is smaller than 1000.")
+
+# 2
+print("In order to create a range of numbers insert two integer numbers")
+var1 = int(input("Insert first number: "))
+var2 = int(input("Insert last number: "))
+print("You inserted", var1, " and ", var2)
+
+prev_x = 0
+for x in range(var1, var2+1):
+    print(x+prev_x)
+    prev_x = x
+
+
