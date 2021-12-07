819f285223e9cab07db88d97169e5851aef3cdf7
AndreaPips96 <a.pipino@studenti.polito.it>
2021-12-07 23:54:18 +0100

    Lab3 completed and commented

--- Lab03/main.py
+++ Lab03/main.py
@@ -1,25 +1,33 @@
 import json
 import pandas as pd
 import numpy as np
-from lab03 import Network
-from lab03 import SignalInformation
+from useful_classes import Network
+from useful_classes import SignalInformation
 
-
+# Open and import JSON file
 f = open('nodes.json')
 nodes_dict = json.load(f)
 f.close()
 # print(nodes_dict)
+
+# Initialize network DataFrame
 network_df = pd.DataFrame()
 
-paths = []
+# Build network
 network = Network(nodes_dict)
+# network.draw()
 network.connect()
+
+# Find all possible paths in the network
+paths = []
 for node1 in network.nodes.keys():
     for node2 in network.nodes.keys():
         if node1 != node2:
             paths += network.find_paths(node1, node2)
+# Insert paths in network DataFrame
 network_df['Path'] = paths
 
+# Evaluate latency, noise power and SNR for each path
 latencies = []
 noise = []
 snr = []
@@ -29,8 +37,8 @@ for path in paths:
     latencies.append(signal.latency)
     noise.append(signal.noise_power)
     snr.append(10*np.log10(signal.signal_power/signal.noise_power))
-
+# Insert latency, noise power and SNR in network DataFrame
 network_df['Latency'] = latencies
 network_df['Noise_power'] = noise
 network_df['SNR'] = snr
-print(network_df)
+# print(network_df)
